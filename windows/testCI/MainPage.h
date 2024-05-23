18f4d64f431151f5c22be4711b9f848b684ad993
TatianaKapos <tatianakapos@microsoft.com>
2024-05-23 15:04:26 -0700

    intital project

---
+++ windows/testCI/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::testCI::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::testCI::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
